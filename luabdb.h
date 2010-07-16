6bfa73ce557c6da177c202b9f8d911415edee9cc
Rob Hoelz <rob@hoelz.ro>
2010-07-16 11:11:33 -0500

    Add metatable constants and utility macros to luabdb.h

--- luabdb.h
+++ luabdb.h
@@ -23,4 +23,20 @@
 #ifndef LUA_BDB_H
 #define LUA_BDB_H
 
+#define LUABDB_DB "DB*"
+#define LUABDB_ENV "DB_ENV*"
+#define LUABDB_CURSOR "DBC*"
+#define LUABDB_LOG "DB_LOGC*"
+#define LUABDB_MEMPOOL "DB_MPOOLFILE*"
+#define LUABDB_SEQ "DB_SEQUENCE*"
+#define LUABDB_TXN "DB_TXN*"
+
+#define luabdb_todb(L, narg) ((DB *) luaL_checkudata(L, narg, LUABDB_DB))
+#define luabdb_toenv(L, narg) ((DB_ENV *) luaL_checkudata(L, narg, LUABDB_ENV))
+#define luabdb_tocursor(L, narg) ((DBC *) luaL_checkudata(L, narg, LUABDB_CURSOR))
+#define luabdb_tolog(L, narg) ((DB_LOGC *) luaL_checkudata(L, narg, LUABDB_LOG))
+#define luabdb_tomempool(L, narg) ((DB_MPOOLFILE *) luaL_checkudata(L, narg, LUABDB_MEMPOOL))
+#define luabdb_toseq(L, narg) ((DB_SEQUENCE *) luaL_checkudata(L, narg, LUABDB_SEQ))
+#define luabdb_totxn(L, narg) ((DB_TXN *) luaL_checkudata(L, narg, LUABDB_TXN))
+
 #endif
